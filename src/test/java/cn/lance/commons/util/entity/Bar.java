755b40c8b2dc1d347e9f5eca304000d7e0571625
Lance <157221693+7ance@users.noreply.github.com>
2025-07-12 23:57:56 +0800

    feat: init

---
+++ src/test/java/cn/lance/commons/util/entity/Bar.java
@@ -0,0 +1,14 @@
+package cn.lance.commons.util.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bar {
+
+    private Integer integer;
+
+}
